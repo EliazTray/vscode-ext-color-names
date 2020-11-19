e32cc300482b4a8444c8744e3fbbbb45197307e1
Eliaztray <944126009@qq.com>
2020-11-19 16:38:57 +0800

    feat: 增加缓存控制和问题反馈

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,76 @@
 import * as vscode from 'vscode';
-import { getColors, parseColor } from './utils';
+import { ColorEntity, getColors, parseColor } from './utils';
+import * as fs from 'fs';
 const closestvector = require('closestvector');
 
 export async function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "color-names" is now active!');
-    const colors = await getColors();
-    const closest = new closestvector(colors.map((c) => [c.rgb.r, c.rgb.g, c.rgb.b]));
+
+    const fileUri = vscode.Uri.joinPath(context.globalStorageUri, 'colors.json');
+    let colors: ColorEntity[] = [];
+    let closest: { get: (arg0: [number, number, number]) => any };
+
+    // if exists, use it before newest data resolved
+    try {
+        if (fs.existsSync(fileUri.path)) {
+            const filecontent = Buffer.from(await vscode.workspace.fs.readFile(fileUri)).toString('utf8');
+            colors = JSON.parse(filecontent);
+        }
+    } catch (error) {
+        vscode.window.showErrorMessage(error.message);
+    }
+
+    if (colors.length === 0) {
+        try {
+            colors = await getColors();
+            closest = new closestvector(colors.map((c) => [c.rgb.r, c.rgb.g, c.rgb.b]));
+            vscode.workspace.fs.writeFile(fileUri, Buffer.from(JSON.stringify(colors)));
+        } catch (error) {
+            vscode.window.showErrorMessage(error.message);
+        }
+    } else {
+        // not blocked
+        getColors().then((result) => {
+            colors = result;
+            closest = new closestvector(colors.map((c) => [c.rgb.r, c.rgb.g, c.rgb.b]));
+            vscode.workspace.fs.writeFile(fileUri, Buffer.from(JSON.stringify(colors)));
+            console.log('"color-names" update data success!');
+        });
+    }
+
     console.log('"color-names" get data ready');
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('color-names.generateColorNames', async () => {
-        // The code you place here will be executed every time your command is executed
         const text = await vscode.env.clipboard.readText();
         try {
             const nearestColor = closest.get(parseColor(text));
             const result = colors[nearestColor['index']].name;
             vscode.env.clipboard.writeText(result);
-            vscode.window.showTextDocument;
-            vscode.window.showInformationMessage(`The ColorName ${result} is copied!`);
+            vscode.window.showInformationMessage(`The ColorName # ${result} # is copied!`);
         } catch (error) {
-            vscode.window.showErrorMessage(`clipbord data ${text.slice(0, 10)}... isn\'t valid color`, 'feedback');
+            vscode.window
+                .showErrorMessage(
+                    `The text \`${
+                        text.length > 7 ? text.slice(0, 6) + '...' : text
+                    }\` on the clipboard is not a valid color value`,
+                    'Feedback'
+                )
+                .then((selected) => {
+                    if (selected === 'Feedback') {
+                        vscode.commands.executeCommand('color-names.feedback');
+                    }
+                });
         }
     });
 
     context.subscriptions.push(disposable);
+
+    vscode.commands.registerCommand('color-names.feedback', async () => {
+        vscode.env.openExternal(vscode.Uri.parse('https://github.com/EliazTray/vscode-ext-color-names/issues'));
+    });
 }
 
 // this method is called when your extension is deactivated
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-interface ColorEntity {
+export interface ColorEntity {
     name: string;
     hex: string;
     rgb: { r: number; g: number; b: number };
@@ -19,23 +19,11 @@ export function parseColor(hexText: string): [number, number, number] {
         const str = hexMatch[1];
         let result;
         if (str.length === 3) {
-            result = [
-                str.charAt(0).repeat(2),
-                str.charAt(1).repeat(2),
-                str.charAt(2).repeat(2),
-            ] as const;
+            result = [str.charAt(0).repeat(2), str.charAt(1).repeat(2), str.charAt(2).repeat(2)] as const;
         } else {
-            result = [
-                str.slice(0, 2),
-                str.slice(2, 4),
-                str.slice(4, 6),
-            ] as const;
+            result = [str.slice(0, 2), str.slice(2, 4), str.slice(4, 6)] as const;
         }
-        return result.map((hex) => parseInt(hex, 16)) as [
-            number,
-            number,
-            number
-        ];
+        return result.map((hex) => parseInt(hex, 16)) as [number, number, number];
     }
-    throw new Error('not valid color');
+    throw new Error('Not valid color value!');
 }
